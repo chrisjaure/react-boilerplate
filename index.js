6111fbd1ab1d82b3561012d274c685e1fd7eb61c
Chris Jaure <chrisjaure@gmail.com>
2015-02-11 20:03:01 -0600

    gather some useful tools

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Immutable from 'immutable';
+
+class App extends React.Component {
+	render() {
+		return (
+			<h1>App</h1>
+		);
+	}
+}
+
+React.render(<App />, document.getElementById('app'));
