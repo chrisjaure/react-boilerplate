2234fa1f1d888d570ed50249a0b549cc42c60bfd
Chris Jaure <chrisjaure@gmail.com>
2015-06-06 13:14:10 -0500

    add components dir

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 import React from 'react';
-
-class App extends React.Component {
-	render() {
-		return (
-			<h1>App</h1>
-		);
-	}
-}
+import App from './components/app';
 
 React.render(<App />, document.getElementById('app'));
